af431a93b8794e254497746136f3aa39ab0f0c3c
davit.chanturia <chanturiadato@gmail.com>
2024-04-30 17:38:27 +0400

    chore: installing prettier

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { router } from './router.js'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { router } from './router.js';
+import { createPinia } from 'pinia';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const pinia = createPinia();
 const vuetify = createVuetify({
