63d3088e49d86f6bbcd0ff1549e1e04fe603b7d8
davit.chanturia <chanturiadato@gmail.com>
2024-04-30 17:44:31 +0400

    chore: uninstall vuetify

--- src/main.js
+++ src/main.js
@@ -3,19 +3,12 @@ import './style.css';
 import App from './App.vue';
 import { router } from './router.js';
 import { createPinia } from 'pinia';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
 const pinia = createPinia();
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
-app.use(vuetify);
+
 app.mount('#app');
