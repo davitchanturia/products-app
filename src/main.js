cc2306efa41bcd5935056e9cfb124e52cc3cf15e
davit.chanturia <chanturiadato@gmail.com>
2024-04-30 17:20:35 +0400

    chore: installing tailwind css and vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router.js'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const pinia = createPinia()
+const pinia = createPinia();
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.use(vuetify);
+app.mount('#app');
